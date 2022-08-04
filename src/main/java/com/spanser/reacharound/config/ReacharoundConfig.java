310b910ce3f245311549fb0d9d3471931c071e92
exin <me@exin.xyz>
2022-08-04 16:40:57 -0500

    feat(config): add mode config

--- src/main/java/com/spanser/reacharound/config/ReacharoundConfig.java
+++ src/main/java/com/spanser/reacharound/config/ReacharoundConfig.java
@@ -2,6 +2,8 @@ package com.spanser.reacharound.config;
 
 public class ReacharoundConfig {
     public boolean enabled = true;
+    
+    public byte mode = 0;
     public byte indicatorStyle = 0;
 
     public String indicatorVertical = "|   |";
