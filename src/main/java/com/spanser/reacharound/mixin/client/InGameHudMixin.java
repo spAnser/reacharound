d85f16e47c33a3f67f8e0d0913e2bcb23ae15857
spAnser <spenson66@gmail.com>
2022-08-10 15:19:17 -0500

    feat(config): Cleanup config usage.

--- src/main/java/com/spanser/reacharound/mixin/client/InGameHudMixin.java
+++ src/main/java/com/spanser/reacharound/mixin/client/InGameHudMixin.java
@@ -2,6 +2,7 @@ package com.spanser.reacharound.mixin.client;
 
 import com.spanser.reacharound.Reacharound;
 import com.spanser.reacharound.client.feature.PlacementFeature;
+import com.spanser.reacharound.config.ReacharoundConfig;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.util.math.MatrixStack;
@@ -22,8 +23,12 @@ public class InGameHudMixin {
     @Shadow
     private int scaledWidth;
 
+    private ReacharoundConfig config;
+
     @Inject(method = "render", at = @At(value = "TAIL"))
     public void renderPlacementAssistText(MatrixStack matrices, float tickDelta, CallbackInfo ci) {
+        config = Reacharound.getInstance().config;
+
         if (!canReachAround()) {
             return;
         }
@@ -31,23 +36,23 @@ public class InGameHudMixin {
         matrices.push();
         matrices.translate(scaledWidth / 2F, scaledHeight / 2f - 4, 0);
 
-        int duration = Reacharound.getInstance().config.indicatorAnimationDuration;
+        int duration = config.indicatorAnimationDuration;
         float scale;
-        if (Reacharound.getInstance().config.indicatorAnimationDuration > 0) {
+        if (config.indicatorAnimationDuration > 0) {
             scale = Math.min(duration, PlacementFeature.ticksDisplayed + tickDelta) / ((float) (duration));
         } else {
             scale = 1;
         }
 
         float fade;
-        switch (Reacharound.getInstance().config.indicatorAnimationFadeInterpolation) {
+        switch (config.indicatorAnimationFadeInterpolation) {
             case 1 -> fade = scale; // linear
             case 2 -> fade = scale * scale; // quadratic
             case 3 -> fade = scale * scale * scale; // cubic
             default -> fade = 1; // none
         }
 
-        switch (Reacharound.getInstance().config.indicatorAnimationInterpolation) {
+        switch (config.indicatorAnimationInterpolation) {
             case 1 -> scale *= 1; // linear
             case 2 -> scale *= scale; // quadratic
             case 3 -> scale *= scale * scale; // cubic
@@ -57,16 +62,16 @@ public class InGameHudMixin {
 
         int color;
         if (PlacementFeature.canPlace(client.player)) {
-            color = Reacharound.getInstance().config.indicatorColor;
+            color = config.indicatorColor;
         } else {
-            color = Reacharound.getInstance().config.indicatorColorObstructed;
+            color = config.indicatorColorObstructed;
         }
 
         int alpha = (int) ((color >>> 24) * fade);
 
         color = (alpha << 24) | (color & 0x00ffffff);
 
-        switch (Reacharound.getInstance().config.indicatorStyle) {
+        switch (config.indicatorStyle) {
             case 1 -> renderStyleQuark(matrices, color);
             case 2 -> renderStyleCustom(matrices, color);
             default -> renderStyleDefault(matrices, color);
@@ -98,7 +103,7 @@ public class InGameHudMixin {
     }
 
     public void renderStyleCustom(MatrixStack matrices, int color) {
-        String text = PlacementFeature.isVertical() ? Reacharound.getInstance().config.indicatorVertical : Reacharound.getInstance().config.indicatorHorizontal;
+        String text = PlacementFeature.isVertical() ? config.indicatorVertical : config.indicatorHorizontal;
         renderText(matrices, color, text);
     }
 
@@ -108,6 +113,6 @@ public class InGameHudMixin {
     }
 
     private boolean canReachAround() {
-        return Reacharound.getInstance().config.enabled && PlacementFeature.currentTarget != null && client.player != null && client.world != null && client.crosshairTarget != null;
+        return config.enabled && PlacementFeature.currentTarget != null && client.player != null && client.world != null && client.crosshairTarget != null;
     }
 }
