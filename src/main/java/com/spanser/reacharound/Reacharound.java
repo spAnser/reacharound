32da7952018ea78306946c7fb7935387b416665b
spAnser <spenson66@gmail.com>
2024-06-13 14:40:02 -0500

    chore: Change log to parameterization.

--- src/main/java/com/spanser/reacharound/Reacharound.java
+++ src/main/java/com/spanser/reacharound/Reacharound.java
@@ -53,7 +53,7 @@ public class Reacharound implements ClientModInitializer {
                 config = gson.fromJson(fileReader, ReacharoundConfig.class);
                 fileReader.close();
             } catch (IOException e) {
-                LOGGER.warn("Could not load reacharound config: " + e.getLocalizedMessage());
+                LOGGER.warn("Could not load reacharound config: {}", e.getLocalizedMessage());
             }
         } else {
             config = new ReacharoundConfig();
@@ -72,7 +72,7 @@ public class Reacharound implements ClientModInitializer {
             fileWriter.write(gson.toJson(config));
             fileWriter.close();
         } catch (IOException e) {
-            LOGGER.warn("Could not save reacharound config: " + e.getLocalizedMessage());
+            LOGGER.warn("Could not save reacharound config: {}", e.getLocalizedMessage());
         }
     }
 }
